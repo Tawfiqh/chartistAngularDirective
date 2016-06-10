75735d042856f3cfd7af3e6adea073f4058dd209
Tawfiq <tawfiq.hamid@gmail.com>
2016-06-10 14:56:09 +0100

    Fixed a bug with passing options to the directive.

--- src/chartistAngularDirective.js
+++ src/chartistAngularDirective.js
@@ -7,7 +7,7 @@
             return {
                 scope: {
                     data: '=',
-                    options: '@',
+                    options: '=',
                     responsiveOptions: '@',
                     type: '@',
                     id: '@',
