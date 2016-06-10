318cadaea02fb62f46601bb7660940084b7be102
Tawfiqh <tawfiq.hamid@gmail.com>
2016-06-10 12:18:26 +0100

    Updated chartistAngularDirective to use inline functions and 'element' as opposed to JQuery

--- src/chartistAngularDirective.js
+++ src/chartistAngularDirective.js
@@ -1,72 +1,67 @@
-angular
-  .module('chartistAngularDirective', [])
-  .directive('ngChartist', ngChartist);
+(function () {
+    "use strict";
 
-ngChartist.$inject = ['$compile'];
+    angular.module('App')
+        .directive('exgChartist', ['$compile',
+        function ($compile) {
+            return {
+                scope: {
+                    data: '=',
+                    options: '@',
+                    responsiveOptions: '@',
+                    type: '@',
+                    id: '@',
+                    tooltips: '@'
+                },
+                restrict: 'E',
+                link: function (scope, element, attrs) {
 
-function ngChartist($compile) {
-  return {
-    scope: {
-      data: '=',
-      options: '@',
-      responsiveOptions: '@',
-      type: '@',
-      id: '@',
-      tooltips: '@'
-    },
-    link: link,
-    restrict: 'EA'
-  };
+                    var graph = Chartist[scope.type]('#' + scope.id, scope.data, scope.options, scope.responsiveOptions);
 
-  function link(scope) {
+                    // set watcher for future data updates
+                    scope.$watch('data', function (newValue, oldValue) {
+                        if (newValue === oldValue) {
+                            return;
+                        }
+                        graph.update(scope.data, true);
+                    }, true);
 
-    var graph = Chartist[scope.type]('#' + scope.id, scope.data, scope.options, scope.responsiveOptions);
+                    // set watcher for future options update
+                    scope.$watch('options', function (newValue, oldValue) {
+                        if (newValue === oldValue) {
+                            return;
+                        }
+                        graph.update(scope.options, true);
+                    }, true);
 
-    // set watcher for future data updates
-    scope.$watch('data', function(newValue, oldValue) {
-      if(newValue === oldValue) {
-        return;
-      }
-      graph.update(scope.data, true);
-    }, true);
+                    // function to bindToolTips to each value
+                    var bindToolTip = function (scope) {
+                        var $toolTip = element
+                          .append($compile('<div class="tooltip"></div>')(scope))
+                          .find('.tooltip')
+                          .hide();
 
-    // set watcher for future options update
-    scope.$watch('options', function(newValue, oldValue) {
-      if(newValue === oldValue) {
-        return;
-      }
-      graph.update(scope.options, true);
-    }, true);
+                        element.on('mouseenter', '.ct-point', function () {
+                            var $point = $(this);
+                            var value = $point.attr('ct:value');
+                            $toolTip.html(value).show();
+                        });
 
-    // function to bindToolTips to each value
-    var bindToolTip = function (scope) {
-      var $chart = $('#' + scope.id);
-      var $toolTip = $chart
-        .append($compile('<div class="tooltip"></div>')(scope))
-        .find('.tooltip')
-        .hide();
+                        element.on('mouseleave', '.ct-point', function () {
+                            $toolTip.hide();
+                        });
 
-      $chart.on('mouseenter', '.ct-point', function() {
-        var $point = $(this);
-        var value = $point.attr('ct:value');
-        $toolTip.html(value).show();
-      });
+                        element.on('mousemove', function (event) {
+                            $toolTip.css({
+                                left: (event.pageX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
+                                top: (event.pageY || event.originalEvent.layerY) - $toolTip.height() - 40
+                            });
+                        });
 
-      $chart.on('mouseleave', '.ct-point', function() {
-        $toolTip.hide();
-      });
+                    };
 
-      $chart.on('mousemove', function(event) {
-        $toolTip.css({
-          left: (event.pageX || event.originalEvent.layerX) - $toolTip.width() / 2 - 10,
-          top: (event.pageY || event.originalEvent.layerY) - $toolTip.height() - 40
-        });
-      });
-
-    };
-
-    // if parameter in html is set, bind tool tips
-    if(scope.tooltips) bindToolTip(scope);
-  }
-
-}
+                    // if parameter in html is set, bind tool tips
+                    if (scope.tooltips) bindToolTip(scope);
+                }}}]
+    )
+})();
